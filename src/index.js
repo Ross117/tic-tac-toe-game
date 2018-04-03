cbdca1b76187730278dd483683af0fa86aa97557
Ross117 <RossMorran@hotmail.co.uk>
2018-04-03 20:46:47 +0100

    Add HTML foundation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 'use strict';
 
 const game = (function setupGame() {
-  // update in response to user & computer actions
+
   const state = {
-    // remove options when played
     options: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    // noughts or crosses, use the properties to update the board
-    identifers: { user: '', computer: '' },
-    computerPlays: [],
-    userPlays: [],
+    identifers: { computer: '', user: '' },
+    moves: { computer: [], user: [] },
   };
 
   const winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9],
@@ -41,13 +38,13 @@ const game = (function setupGame() {
     return winningCombo;
   };
 
-  // need to alternate turns between user & computer
-
   const computerPlay = () => {
     let computerSelection;
 
+    // get a randon integer lower than a specified maximum
     const getRandomInt = max => Math.floor(Math.random() * max);
 
+    // check if a winning move is available
     const checkForPriorityMv = (playerHist) => {
       let nxtMv = null;
       let i = 1;
@@ -68,11 +65,13 @@ const game = (function setupGame() {
       return nxtMv;
     };
 
+    // if no priority moves are available, pick the best available move
     const pickRegularMv = (playerHist) => {
       let nxtMv = null;
       let i = 1;
       const choices = [];
 
+      // pick the move which leaves the most winning combinations open
       const pickBestOption = (options) => {
         const countArr = [];
 
@@ -112,47 +111,47 @@ const game = (function setupGame() {
     };
 
 
-    if (state.computerPlays.length === 0 && state.computerPlays.length === 0) {
+    if (state.moves.computer.length === 0 && state.moves.computer.length === 0) {
       // 1. at the start of the game, 5 leaves the most winning combos open
       computerSelection = 5;
-    } else if (state.computerPlays.length === 0) {
+    } else if (state.moves.computer.length === 0) {
       // 2. focus on blocking the user already?
-      computerSelection = pickRegularMv(state.userPlays);
+      computerSelection = pickRegularMv(state.moves.users);
     // 3. if computer is just 1 square away, and that square is available, pick that
-    } else if (checkForPriorityMv(state.computerPlays) !== null) {
-      computerSelection = checkForPriorityMv(state.computerPlays);
+    } else if (checkForPriorityMv(state.moves.computer) !== null) {
+      computerSelection = checkForPriorityMv(state.moves.computer);
     // 4. need to block user if they're 1 square away from winning
-    } else if (checkForPriorityMv(state.userPlays) !== null) {
-      computerSelection = checkForPriorityMv(state.userPlays);
-    // 5. if computer can move to being just 1 away from a winning combination still in play,
-    // pick that option. If there's more than 1 possibility,
-    // choose option which leaves open largest amount of winning combinations
-    } else if (pickRegularMv(state.computerPlays) !== null) {
-      computerSelection = pickRegularMv(state.computerPlays);
+    } else if (checkForPriorityMv(state.moves.users) !== null) {
+      computerSelection = checkForPriorityMv(state.moves.users);
+    // 5. if there are no priority moves, pick the best option which moves
+    // the computer closer to winning
+    } else if (pickRegularMv(state.moves.computer) !== null) {
+      computerSelection = pickRegularMv(state.moves.computer);
     // 6. Otherwise, block the best option for the user
-    } else if (pickRegularMv(state.userPlays) !== null) {
-      computerSelection = pickRegularMv(state.userPlays);
+    } else if (pickRegularMv(state.moves.users) !== null) {
+      computerSelection = pickRegularMv(state.moves.users);
+    // 7. else choose a random option
     } else computerSelection = state.options[getRandomInt(state.options.length)];
 
-    // 7. update computerPlays array
-    state.computerPlays.push(computerSelection);
-    // 8. update board
-    // 9. Check if computer has won
-    if (checkforWinner('computer', state.computerPlays)) return;
-    // 10. remove selection from options array
+    // 8. update computerPlays array
+    state.moves.computer.push(computerSelection);
+    // 9. update board
+    // 10. Check if computer has won
+    if (checkforWinner('computer', state.moves.computer)) return;
+    // 11. remove selection from options array
     const ind = state.options.indexOf(computerSelection);
     state.options.splice(ind, 1);
-  // 11. If computer has not won, switch to user
+  // 12. If computer has not won, switch to user
   };
 
   // make sure user can only select one of the remaining options
   // handle this in HTML I think, disable taken squares
   const userPlay = (userSelection) => {
     // 1. update userPlays array
-    state.userPlays.push(userSelection);
+    state.moves.users.push(userSelection);
     // 2. update board
     // 3. check if user has won
-    if (checkforWinner('user', state.userPlays)) return;
+    if (checkforWinner('user', state.moves.users)) return;
     // 4. remove selection from options array
     const ind = state.options.indexOf(userSelection);
     state.options.splice(ind, 1);
