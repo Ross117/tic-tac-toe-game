d8a666a8979085c39ef3d7dce1ea7e80d1d555cc
Ross117 <RossMorran@hotmail.co.uk>
2018-04-04 21:56:06 +0100

    Add user on click event handling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 'use strict';
 
 const game = (function setupGame() {
-
   const state = {
     options: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    identifers: { computer: '', user: '' },
+    identifers: { computer: 'O', user: 'X' },
     moves: { computer: [], user: [] },
   };
 
   const winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9],
     [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]];
-
-  // board HTMl element
-  const $board = document.querySelector('.board');
 
   // fires when user clicks 'play' button
   const selectIdentifier = () => {
@@ -111,50 +107,57 @@ const game = (function setupGame() {
     };
 
 
-    if (state.moves.computer.length === 0 && state.moves.computer.length === 0) {
+    if (state.moves.computer.length === 0 && state.moves.user.length === 0) {
       // 1. at the start of the game, 5 leaves the most winning combos open
       computerSelection = 5;
     } else if (state.moves.computer.length === 0) {
       // 2. focus on blocking the user already?
-      computerSelection = pickRegularMv(state.moves.users);
+      computerSelection = pickRegularMv(state.moves.user);
     // 3. if computer is just 1 square away, and that square is available, pick that
     } else if (checkForPriorityMv(state.moves.computer) !== null) {
       computerSelection = checkForPriorityMv(state.moves.computer);
     // 4. need to block user if they're 1 square away from winning
-    } else if (checkForPriorityMv(state.moves.users) !== null) {
-      computerSelection = checkForPriorityMv(state.moves.users);
+    } else if (checkForPriorityMv(state.moves.user) !== null) {
+      computerSelection = checkForPriorityMv(state.moves.user);
     // 5. if there are no priority moves, pick the best option which moves
     // the computer closer to winning
     } else if (pickRegularMv(state.moves.computer) !== null) {
       computerSelection = pickRegularMv(state.moves.computer);
     // 6. Otherwise, block the best option for the user
-    } else if (pickRegularMv(state.moves.users) !== null) {
-      computerSelection = pickRegularMv(state.moves.users);
+    } else if (pickRegularMv(state.moves.user) !== null) {
+      computerSelection = pickRegularMv(state.moves.user);
     // 7. else choose a random option
     } else computerSelection = state.options[getRandomInt(state.options.length)];
 
-    // 8. update computerPlays array
+    // 8. update board
+    document.querySelector(`[id='${computerSelection}']`).innerText = state.identifers.computer;
+    // 9. update computerPlays array
     state.moves.computer.push(computerSelection);
-    // 9. update board
     // 10. Check if computer has won
     if (checkforWinner('computer', state.moves.computer)) return;
     // 11. remove selection from options array
     const ind = state.options.indexOf(computerSelection);
     state.options.splice(ind, 1);
-  // 12. If computer has not won, switch to user
   };
 
-  // make sure user can only select one of the remaining options
-  // handle this in HTML I think, disable taken squares
-  const userPlay = (userSelection) => {
-    // 1. update userPlays array
-    state.moves.users.push(userSelection);
-    // 2. update board
+  const userPlay = (event) => {
+    const $targetDiv = event.srcElement;
+
+    // 1. update board
+    if ($targetDiv.innerText === '') $targetDiv.innerText = state.identifers.user;
+    else return;
+    const numericVal = parseInt($targetDiv.id, 10);
+    // 2. update userPlays array
+    state.moves.user.push(numericVal);
     // 3. check if user has won
-    if (checkforWinner('user', state.moves.users)) return;
+    if (checkforWinner('user', state.moves.user)) return;
     // 4. remove selection from options array
-    const ind = state.options.indexOf(userSelection);
+    const ind = state.options.indexOf(numericVal);
     state.options.splice(ind, 1);
+
+    // console.log(state.moves.user);
+    // console.log(state.options);
+
     // 5. make computer play
     computerPlay();
   };
@@ -165,4 +168,8 @@ const game = (function setupGame() {
   };
 }());
 
-// attach event listeners
+const sqrs = document.querySelectorAll('.sqr');
+
+Array.from(sqrs).forEach((sqr) => {
+  sqr.addEventListener('click', game.userPlay);
+});
