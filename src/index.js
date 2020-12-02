20dd9b5bb6afdc27844516205d68f7197617df13
Ross <rossmorran@hotmail.co.uk>
2020-12-02 16:00:40 +0000

    Add IE11 Support
    I've used Babel & core-js CDN.
    
    TODO:
      - Add Webpack

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const game = (function setupGame() {
   };
 
   const winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9],
-    [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]];
-
+    [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]]; 
   const $sqrs = Array.from(document.querySelectorAll('.sqr'));
   const $msg = document.querySelector('.msg');
 
